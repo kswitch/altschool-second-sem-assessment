a2ef954c1bb978af289be76e9a0b67c23172a650
Kingsley Osuagwu-Chidiadi <chidiadi.kingsley@gmail.com>
2023-10-03 23:49:13 +0100

    Minor cleanup and edit

--- routes/admin.js
+++ routes/admin.js
@@ -9,14 +9,16 @@ const getAllItems = require('../middleware/getAllItems');
 // admin/items
 router.get('/items', checkApiKey, getAllItems)
 
+// admin/add-item
 router.post('/add-item', checkApiKey, validateEntry, addItem);
 
+// admin/delete-item
 router.delete('/delete-item', checkApiKey, deleteItem);
 
+
+// admin/update-item
 router.put('/update-item', checkApiKey, (req, res, next) => {
     return res.status(204).json({message: 'Entry Updated Successfully'})
 })
-
-
 
 module.exports = router
